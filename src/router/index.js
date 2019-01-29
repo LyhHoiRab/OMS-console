a9d52b1f22a3da24e900f928e384d00f5037126c
HSL <HSL@HSLdeiMac-3.local>
2019-01-29 15:23:12 +0800

    测试页

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
+const Test = {
+  template: `<div>Hello Wah !!</div>`
+}
+
 export default new Router({
-  routes: [
-    {
+  routes: [{
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+      component: Test,
+  }]
 })
